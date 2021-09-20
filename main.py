1a994e5297116add992f1aa7f921f185926044a8
Netanel Haber <58652339+netanel-haber@users.noreply.github.com>
2021-09-20 10:56:05 +0000

    removed Session for simpler code, less requests

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-from requests import Session
+import requests
 from time import time
 from concurrent.futures import ThreadPoolExecutor
 
-NUM_REQUESTS = 100
+NUM_REQUESTS = 5
 
 
 def url(i):
     return f"https://jsonplaceholder.typicode.com/todos/{i}"
 
 
-def get_fetcher(session):
-    return lambda i: session.get(url(i)).json()["id"]
+def fetch(i):
+    return requests.get(url(i)).json()["id"]
 
 
 def single_threaded():
-    with Session() as session:
-        return list(map(get_fetcher(session), range(1, NUM_REQUESTS + 1)))
+    return list(map(fetch, range(1, NUM_REQUESTS + 1)))
 
 
 def multi_threaded():
     with ThreadPoolExecutor(max_workers=NUM_REQUESTS) as executor:
-        with Session() as session:
-            return list(
-                executor.map(get_fetcher(session), range(1, NUM_REQUESTS + 1)))
+        return list(executor.map(fetch, range(1, NUM_REQUESTS + 1)))
 
 
 def execute(func, name):
